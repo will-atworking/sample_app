e3e40339aad633d8cd39943307dc2e0178840350
Will Fleming <will@atworking.com>
2014-04-25 12:06:13 +0900

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,13 @@ require 'spec_helper'
 describe "StaticPages" do
 
 	describe "Home page" do
-
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
+		end
+		it "Should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
 		end
 	end
 
@@ -15,13 +18,20 @@ describe "StaticPages" do
 			visit '/static_pages/help'
 			expect(page).to have_content("Help")
 		end
+		it "Should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 
 	describe "About Page" do
-
 		it "Should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content("About Us")
+		end
+		it "Should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
 		end
 	end
 
