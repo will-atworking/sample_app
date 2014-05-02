6022abf0dafbb2869c252e369d49f70db1477795
Will Fleming <will@atworking.com>
2014-05-02 14:26:17 +0900

    Completed initial section

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,16 @@ describe "User pages" do
 
 	subject { page }
 
-	describe "signup page" do
+	describe "profile page" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
 
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
+
+	end
+
+	describe "signup page" do
 		before { visit signup_path }
 
 		it { should have_content('Sign up') }
